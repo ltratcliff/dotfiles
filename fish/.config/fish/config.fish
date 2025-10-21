32a2709a23796026b6f0095dedfa9f466dc9f8ca
Tom Ratcliff <ltratcliff@gmail.com>
2025-10-21 09:36:16 -0400

    add nix path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+fish_add_path ~/.nix-profile/bin
 set -gx EDITOR nvim
 zoxide init fish | source
 starship init fish | source
